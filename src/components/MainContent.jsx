60aadeedb60f9c38b44e9db84e2f549996f802dc
Caiohvboas <116687690+Caiohvboas@users.noreply.github.com>
2025-04-04 00:30:33 -0300

    Finalizado, agora é editar e frazer algumas coisas

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,11 +1,17 @@
+import AboutContainer from "./AboutContainer";
+import ProjectsContainer from "./ProjectsContainer";
+import TechnologiesContainer from "./TechnologiesContainer";
 
 import "../styles/components/maincontent.sass";
-import React from 'react'
 
 const MainContent = () => {
   return (
-    <main id="main-content">MainContent</main>
-  )
-}
+    <main id="main-content">
+      <AboutContainer />
+      <TechnologiesContainer />
+      <ProjectsContainer />
+    </main>
+  );
+};
 
-export default MainContent
+export default MainContent;
